7be79b219b5f96fb21e313375fb0e6ea9c77e734
Keith Hand <keith@clevyr.com>
2020-03-03 16:06:36 -0600

    :tada: Initial commit
    First attempt at setting all the outputs needed. Provides the projectid, cluster, region, docker repo url, docker user, and docker password.

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+gcloud auth activate-service-account --key-file - <<< $GCLOUD_KEY_FILE
+
+# Get variables from gcloud
+PROJECT_ID=$(gcloud projects list --format json | jq '.[]["projectId"]' | xargs)
+CLUSTER=$(gcloud container clusters list --format json | jq '.[]["name"]' | xargs)
+REGION=$(gcloud container clusters list --format json | jq '.[]["zone"]' | xargs)
+DOCKER_PASS=$(gcloud auth print-access-token)
+
+# Set all outputs
+printf "::set-output name=gcloud_projectId::%s\n" "$PROJECT_ID"
+printf "::set-output name=gcloud_cluster::%s\n" "$CLUSTER"
+printf "::set-output name=gcloud_region::%s\n" "$REGION"
+printf "::set-output name=docker_repo::%s\n" "us.gcr.io/$PROJECT_ID"
+printf "::set-output name=docker_username::%s\n" "oauth2accesstoken"
+printf "::set-output name=docker_password::%s\n" "$DOCKER_PASS"
